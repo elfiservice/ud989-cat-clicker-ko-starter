83a5ff9d6b3e93c5d718383181bc38af5a77f8d6
elfiservice <armandojunior@Armandos-MacBook-Pro.local>
2017-06-15 11:27:01 -0300

    using With-$parent-add more Cats-listing cats on DOM

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Taddy');
-  this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-  this.nicknames = ko.observableArray(['Tabtab', 'Mr. T']);
+var initialCats = [
+  {
+    clickCount: 0,
+    name: "Taddy",
+    imgSrc: "img/22252709_010df3379e_z.jpg",
+    nicknames: ['Juju', 'Toto']
+  },
+  {
+    clickCount: 0,
+    name: "Xanii",
+    imgSrc: "img/434164568_fea0ad4013_z.jpg",
+    nicknames: ['Arara', 'Coto', 'rubi']
+  },
+  {
+    clickCount: 0,
+    name: "Sammy",
+    imgSrc: "img/9648464288_2516b35537_z.jpg",
+    nicknames: ['Smmmm']
+  }
+];
+
+var Cat = function(dataCats) {
+  this.clickCount = ko.observable(dataCats.clickCount);
+  this.name = ko.observable(dataCats.name);
+  this.imgSrc = ko.observable(dataCats.imgSrc);
+  this.nicknames = ko.observableArray(dataCats.nicknames);
 
   this.nivel = ko.computed(function(){
     var title;
@@ -19,12 +40,26 @@ var Cat = function() {
 };
 
 var ViewModel = function() {
+  //acessar o THis mais Externo
+  var self = this;
 
-  this.currentCat = ko.observable( new Cat());
+  this.catList = ko.observableArray([]);
+  initialCats.forEach(function(catObj){
+    self.catList.push( new Cat(catObj) );
+  });
+
+  this.currentCat = ko.observable( this.catList()[0] );
 
   this.incrementCounter = function() {
-    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
   };
+
+  //no DOM usa o $parent Para subir fora do contexto do CatObj para o ViewModel
+  //clickedCatObj: Na Documentacao diz Q ao Clicar em Algo no DOM com bind
+  //ele executa uma Funcao passando um OBJ q no qual ele esta Ligado
+  this.setCat = function(clickedCatObj) {
+    self.currentCat(clickedCatObj);
+  }
 }
 
 ko.applyBindings(new ViewModel());
